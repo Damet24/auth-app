714fa1286bc2e4fd72c108180dabcc7f414434af
Daniel Mercado <damet.2401@gmail.com>
2026-02-13 23:05:21 -0500

    user routes mapped

--- src/Modules/Users/Routes.js
+++ src/Modules/Users/Routes.js
@@ -1,11 +1,52 @@
 import { authenticate } from '../../Middlewares/Authenticate.js';
 import { authorize } from '../../Middlewares/Authorize.js';
-import { listUsers } from './UserController.js';
+
+import {
+  listUsers,
+  getUser,
+  createUser,
+  updateUser,
+  deactivateUser,
+} from './UserController.js';
 
 export default async function userRoutes(fastify) {
   fastify.get(
     '/',
-    { preHandler: [authenticate, authorize('manage_users')] },
+    {
+      preHandler: [authenticate, authorize('manage_users')],
+    },
     listUsers
+  );
+
+  fastify.get(
+    '/:id',
+    {
+      preHandler: [authenticate, authorize('manage_users')],
+    },
+    getUser
+  );
+
+  fastify.post(
+    '/',
+    {
+      preHandler: [authenticate, authorize('manage_users')],
+    },
+    createUser
+  );
+
+  fastify.patch(
+    '/:id',
+    {
+      preHandler: [authenticate, authorize('manage_users')],
+    },
+    updateUser
+  );
+
+  fastify.delete(
+    '/:id',
+    {
+      preHandler: [authenticate, authorize('manage_users')],
+    },
+    deactivateUser
   );
 }
